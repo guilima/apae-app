74e990d3dab7a456c6993568f35d3619c97313b1
Guilherme Carvalho Lima <gui.lima30@gmail.com>
2014-09-14 03:26:42 -0300

    common

--- js/myfunction.smoothState.js
+++ js/myfunction.smoothState.js
@@ -12,6 +12,27 @@
                         scrollTop: 0
                     });
                 }
+            },
+            callback : function(url, $container, $content) {
+                pageScripts();
             }
+            
         }).data('smoothState');
 })(jQuery);
+
+
+
+function pageScripts(){
+ 	var pageId = document.title;
+	if(pageId == 'APAE | Eventos'){
+
+
+	var tag = document.createElement('script');
+    tag.src = "js/vendors/jquery.calendario.js";
+    var tag2 = document.createElement('script');
+    tag2.src = "js/data.js";
+    var firstScriptTag = document.getElementsByTagName('script')[3];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    firstScriptTag.parentNode.insertBefore(tag2, firstScriptTag);
+}
+}
